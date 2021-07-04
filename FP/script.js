25fa8aa6beed16781d2368a3cf1a6b6b336ba2d3
akhyarz <61868811+akhyarz@users.noreply.github.com>
2021-07-05 00:01:23 +0700

    Add files via upload

--- FP/script.js
+++ FP/script.js
@@ -1,4 +1,4 @@
-//sidebar
+// sidebar
 $('.menu-btn').click(function(){
     $(this).toggleClass('click');
     $('.sidebar').toggleClass('show');
@@ -13,12 +13,16 @@ const checkbox = document.querySelector('input[type=checkbox]');
 const search = document.querySelector('.search input');
 const containerCard = document.querySelector('.cardcontainer');
 const banner = document.querySelector('.banner');
+const btnLog = document.querySelector('.account a');
+//favorite
+const cardfavorite = document.querySelector('.cardfavorite');
 //recipe page
 const recipeInfo = document.querySelector('#recipe_info');
 const recipeDetail = document.querySelector('#recipeDetail');
 //login
 btn.addEventListener('click', validasi);
 checkbox.addEventListener('click',check);
+
 
 //Index
 search.addEventListener('keyup',searchFood);
@@ -30,16 +34,26 @@ document.addEventListener('click',(a) => {
         foodInformation();
     } else if (a.target.classList == 'detailbtn'){
         window.location.href = 'recipe.html';
+    }else if (a.target.classList.value == 'ri-heart-add-fill') {
+        cekLike();
     }
 });
 
 run();
 foodInformation();
+cekLoginOrNot();
+loginLogout();
+
 
 async function run() {
-    const values = await getDataResep();
-    showCard(values);
-    banner.style.display = "block";
+    try {
+        const values = await getDataResep();
+        showCard(values);
+        banner.style.display = "block";
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
@@ -61,10 +75,8 @@ async function validasi() {
             alert('email atau password salah');
         }else {
             alert('Login berhasil');
-            
             window.location.href = 'index.html';
-            let loginA = true;
-            console.log(loginA);
+            localStorage.setItem('idUser',data.values[0]._id);
         }
     }
 }
@@ -87,7 +99,120 @@ async function login(){
     return newData;
 }
 
-//Index
+//Index page
+
+function cekLoginOrNot() {
+    const user = localStorage.getItem('idUser');
+    if (user != null) {
+        btnLog.innerText = 'Log Out';
+        loginLogout();
+        getFavoriteUser();
+    }else {
+        try {
+            cardfavorite.innerHTML = `<div class="error">
+            <img src="https://cdn.discordapp.com/attachments/764062693124341760/861212466134646824/Pngtree_man_with_sadness_face_concept_5311454-removebg-preview.png" alt="" srcset="">
+            <h2>Sorry you must login first</h2>
+            </div>`;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+}
+
+function loginLogout() {
+    try {
+        btnLog.addEventListener('click',() => {
+            if (btnLog.innerText == 'Log Out'){
+                localStorage.removeItem('idUser');
+                window.location.href = 'index.html';
+            }else if (btnLog.innerText == 'login'){
+                window.location.href = 'login.html';
+            }
+        } );
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function cekLike() {
+    const user = localStorage.getItem('idUser');
+    if (user != null) {
+        likeUser();
+
+    }else {
+        alert('Anda harus login terlebih dahulu');
+    }
+}
+
+function likeUser(val) {
+    try {
+        const liked = document.querySelectorAll('.ri-heart-add-fill');
+        
+        liked.forEach(el => {
+            el.addEventListener('click', () => {
+                if(el.classList.contains('like')){
+                    // deleteFavorite(el.id);
+                    el.classList.remove('like');
+                    console.log('gagal');
+                }else {
+                    el.classList.add('like');
+                    // postFavorit(el.id);
+                    console.log('berhasil');
+                }
+            });
+        });
+        saveLikeUser(liked,val);
+    } catch (error) {
+        console.log(error);
+    }
+}
+function saveLikeUser(liked,val) {
+    try {
+        liked.forEach(el => {
+            if(val.includes(el.id)){
+                el.classList.add('like');
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteFavorite(key) {
+    const idUser = localStorage.getItem('idUser');
+    let userData = {
+        id:`${idUser}`,
+        key :`${key}`
+    };
+
+    let config = {
+        method : "PATCH",
+        body : JSON.stringify(userData),
+        headers : {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+    };
+    await fetch(`https://calm-refuge-58943.herokuapp.com/deleteUserFavorite/`,config);
+
+}
+
+async function postFavorit(key) {
+    const idUser = localStorage.getItem('idUser');
+    let userData = {
+        id:`${idUser}`,
+        key :`${key}`
+    };
+
+    let config = {
+        method : "PATCH",
+        body : JSON.stringify(userData),
+        headers : {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+    };
+    await fetch(`https://calm-refuge-58943.herokuapp.com/updateUser/`,config);
+}
 
 function searchFood(e) {
     if(e.key == "Enter"){
@@ -98,6 +223,7 @@ function searchFood(e) {
         }else {
             banner.style.display = "none";
             searchDataResep(search.value);
+            
         }
         
     }
@@ -127,36 +253,11 @@ async function showCard(values) {
         try {
             containerCard.innerHTML = card;
         } catch (error) {
-            
+            console.log(error);
         }
     }
 }
 
-function cardInfo(el) {
-    return ` <div class="card-food">
-                <div class="header-card">
-                    <div class="img">
-                        <img src=${el.thumb} alt="">
-                    </div>
-                    <div class="detail">
-                        <p>${el.title}</p>
-                        <p>${el.origin}</p>
-                    </div>
-                </div>
-                <div class="info-card">
-                    <div class="inf">
-                        <p>${el.description}</p>
-                    </div>
-                </div>
-                <div class="btn-detail">
-                    <button  class="detail_btn" id="${el.key}"><a id="${el.key}" class="link" >Detail</a></button>
-                    <div class="time">
-                        <button><i class="ri-timer-line"></i><span>${el.times}<span></button>
-                        <button><i class="ri-heart-add-line"></i></button>
-                    </div>
-                </div>  
-            </div> `;
-}
 
 //recipe page
 //data recipe dan recipe derail
@@ -170,7 +271,6 @@ async function foodInformation() {
         showFoodInfo(values);
         showRecipeDetail(values);
         mapBahan(values.ingredient,values.step);
-        console.log(values);
     } catch (er) {
         console.log(er);
         recipeInfo.innerHTML = error();
@@ -186,6 +286,105 @@ async function showFoodInfo(values) {
         
     } catch (error) {
         
+    }
+}
+
+
+//recipe detail page
+async function showRecipeDetail(values) {
+    try {
+        card = '';
+        card += cardRecipeDetail(values);
+        recipeDetail.innerHTML = card;
+    } catch (error) {
+        
+    }
+}
+
+function mapBahan(bahan,langkah) {
+    try {
+        const ingredient = document.querySelector('.ingredient');
+        const step = document.querySelector('.step');
+
+        const listBahan = bahan.map(el => `<li>${el}</li>`).join('');
+        const listStep = langkah.map(el => `<li>${el}</li>`).join('');
+
+        ingredient.innerHTML = listBahan;
+        step.innerHTML = listStep;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//fovorite package
+
+async function likeFavoriteAndDel(val) {
+    const liked = document.querySelectorAll('.ri-heart-add-fill');
+    liked.forEach(el => {
+        el.addEventListener('click',() => {
+            deleteFavorite(el.id);
+            el.classList.remove('like');
+        });
+    });
+    showLike(liked,val);
+}
+
+function showLike(liked,val) {
+    try {
+        for (let i = 0; i < val.length; i++) {
+            if(val[i].key == liked[i].id){
+                liked[i].classList.add('like');
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function getFavoriteUser() {
+    try {
+        const id = localStorage.getItem('idUser');
+
+        const data = await fetch(`https://calm-refuge-58943.herokuapp.com/userById/${id}`);
+        const newData = await data.json();
+        const {values} = newData;
+            
+        getCardFavorite(values.favorit);
+        likeUser(values.favorit); 
+        // dataFavoriteNull(values.favorit);
+    } catch (error) {
+        console.log(error);
+    }
+}
+function dataFavoriteNull(favorit) {
+    if (favorit == '') {
+        card = '';
+        card += error();
+        cardfavorite.innerHTML = card;
+    }
+}
+async function getCardFavorite(val) {
+    let dataFavorite =  [];
+    for (let i = 0; i < val.length; i++) {
+        const data = await  fetch(`https://calm-refuge-58943.herokuapp.com/getResepByKey/${val[i]}`);
+        const newData =  await data.json();
+        const {values} = newData;
+        
+        dataFavorite.push(values);
+    }
+    showCardFavorite(dataFavorite);
+    likeFavoriteAndDel(dataFavorite);
+}
+
+function showCardFavorite(val) {
+    try {
+        card = '';
+        val.forEach(el => card +=  cardInfo(el));
+        cardfavorite.innerHTML = card;
+    } catch (error) {
+        console.log(error);
     }
 }
 
@@ -210,16 +409,6 @@ function foodInfo(val) {
                         <button class="detailbtn">Detail Recipe</button>
                     </div>
                 </div>`;
-}
-//recipe detail page
-async function showRecipeDetail(values) {
-    try {
-        card = '';
-        card += cardRecipeDetail(values);
-        recipeDetail.innerHTML = card;
-    } catch (error) {
-        
-    }
 }
 
 function cardRecipeDetail(val) {
@@ -246,20 +435,35 @@ function cardRecipeDetail(val) {
             </div>`;
 }
 
-function mapBahan(bahan,langkah) {
-    try {
-        const ingredient = document.querySelector('.ingredient');
-        const step = document.querySelector('.step');
-
-        const listBahan = bahan.map(el => `<li>${el}</li>`).join('');
-        const listStep = langkah.map(el => `<li>${el}</li>`).join('');
-
-        ingredient.innerHTML = listBahan;
-        step.innerHTML = listStep;
-    } catch (error) {
-        
-    }
+function cardInfo(el) {
+    return ` <div class="card-food">
+                <div class="header-card">
+                    <div class="img">
+                        <img src=${el.thumb} alt="">
+                    </div>
+                    <div class="detail">
+                        <p>${el.title}</p>
+                        <p>${el.origin}</p>
+                    </div>
+                </div>
+                <div class="info-card">
+                    <div class="info">
+                        <p>${el.description}</p>
+                    </div>
+                </div>
+                <div class="btn-detail">
+                    <button  class="detail_btn" id="${el.key}"><a id="${el.key}" class="link" >Detail</a></button>
+                    <div class="time">
+                        <button><i class="ri-timer-line"></i><span>${el.times}<span></button>
+                        <button><i id="${el.key}" class="ri-heart-add-fill"></i></button>
+                    </div>
+                </div>  
+            </div> `;
 }
+
+
+
+
 //error
 function error() {
     return `<div class="notFound">
